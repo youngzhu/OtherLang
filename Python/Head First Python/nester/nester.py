c80820f569337ffe4d06ecd01081dbeebac369dd
Young-PC <youngzhu321@163.com>
2014-01-28 09:54:56 +0800

    About Python

---
+++ Python/Head First Python/nester/nester.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+""" 递归调用
+"""
+def print_lol(the_list):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item)
+		else:
+			print(each_item)
---
+++ Python/Head First Python/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'Young',
+	author_email = '',
+	url = '',
+	description = '',
+)
