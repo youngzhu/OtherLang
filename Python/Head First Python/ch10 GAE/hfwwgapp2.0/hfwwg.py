9138d44819d852b51b20e71127b16e716852b222
Young-PC <youngzhu321@163.com>
2014-05-09 10:36:13 +0800

    deploy on GAE

--- Python/Head First Python/ch10 GAE/hfwwgapp2.0/hfwwg.py
+++ Python/Head First Python/ch10 GAE/hfwwgapp2.0/hfwwg.py
@@ -2,7 +2,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-from google.appengine.ext.db import djangoforms
+import djangoforms
 
 from google.appengine.api import users
 
